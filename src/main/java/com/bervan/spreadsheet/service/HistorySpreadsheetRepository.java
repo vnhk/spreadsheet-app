f2c7b19868ccb976eb8088996a9565d8ec4a185e
berluk12@gmail.com <berluk12@gmail.com>
2024-11-23 23:59:43 +0100

    PoC HTMLDynamicTable Spreadsheet - show history

--- src/main/java/com/bervan/spreadsheet/service/HistorySpreadsheetRepository.java
+++ src/main/java/com/bervan/spreadsheet/service/HistorySpreadsheetRepository.java
@@ -3,7 +3,9 @@ package com.bervan.spreadsheet.service;
 import com.bervan.history.model.BaseRepository;
 import com.bervan.spreadsheet.model.HistorySpreadsheet;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface HistorySpreadsheetRepository extends BaseRepository<HistorySpreadsheet, UUID> {
+    List<HistorySpreadsheet> findAllByHistoryOwnerId(UUID id);
 }
