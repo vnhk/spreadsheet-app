7d40fcf611f818b1970d156fa7961dfd192ab419
berluk12@gmail.com <berluk12@gmail.com>
2024-12-25 15:08:15 +0100

    Navigation menu addButtonIfVisible

--- src/main/java/com/bervan/spreadsheet/view/SpreadsheetPageLayout.java
+++ src/main/java/com/bervan/spreadsheet/view/SpreadsheetPageLayout.java
@@ -6,9 +6,9 @@ public class SpreadsheetPageLayout extends MenuNavigationComponent {
     public SpreadsheetPageLayout(boolean isEdit, String spreadsheetName) {
         super(AbstractSpreadsheetsView.ROUTE_NAME);
 
-        addButton(menuButtonsRow, AbstractSpreadsheetsView.ROUTE_NAME, "List");
+        addButtonIfVisible(menuButtonsRow, AbstractSpreadsheetsView.ROUTE_NAME, "List");
         if (isEdit) {
-            addButton(menuButtonsRow, AbstractSpreadsheetView.ROUTE_NAME, spreadsheetName);
+            addButtonIfVisible(menuButtonsRow, AbstractSpreadsheetView.ROUTE_NAME, spreadsheetName);
         }
 
         add(menuButtonsRow);
