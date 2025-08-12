722fa973cbe95fea27cbbbabb4c8bdc47dd66027
berluk12@gmail.com <berluk12@gmail.com>
2025-08-12 20:37:05 +0200

    SAVINGS_M_CONTR functions

--- src/main/java/com/bervan/spreadsheet/functions/SavingsWithMonthlyContribution.java
+++ src/main/java/com/bervan/spreadsheet/functions/SavingsWithMonthlyContribution.java
@@ -1,57 +1,54 @@
-//package com.bervan.spreadsheet.functions;
-//
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//
-//@Component
-//public class SavingsWithMonthlyContribution implements SpreadsheetFunction {
-//    @Override
-//    public String calculate(List<String> allParams, List<List<Cell>> rows) {
-//        try {
-//            List<Object> paramsCareAboutOrder = getParams_careAboutOrder(allParams, rows);
-//
-//            if (paramsCareAboutOrder.size() != 4) {
-//                throw new RuntimeException("Incorrect Params");
-//            }
-//
-//            double initialCapital = getDouble(paramsCareAboutOrder, 0);
-//            double interestRate = getDouble(paramsCareAboutOrder, 1);
-//            double monthlyContribution = getDouble(paramsCareAboutOrder, 2);
-//            double years = getDouble(paramsCareAboutOrder, 3);
-//
-//            return String.valueOf(calculateFutureValueWithMonthlyContributions(initialCapital, monthlyContribution, interestRate, years));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return "ERROR!";
-//        }
-//    }
-//
-//    private  double calculateFutureValueWithMonthlyContributions(double initialCapital, double monthlyContribution, double annualInterestRate, double years) {
-//        double monthlyRate = annualInterestRate / 12;
-//        double totalMonths = years * 12;
-//        double futureValue = initialCapital * Math.pow(1 + monthlyRate, totalMonths);
-//
-//        for (int i = 1; i <= totalMonths; i++) {
-//            futureValue += monthlyContribution * Math.pow(1 + monthlyRate, totalMonths - i);
-//        }
-//
-//        return futureValue;
-//    }
-//
-//    @Override
-//    public String getInfo() {
-//        return """
-//                Examples: <br>
-//                Total savings after 10 years with initial capital 10000 and 5% interest rate and 500 monthly contribution <br>
-//                    (a) =SAVINGS_M_CONTR(10000,0.05,500,10) <br>
-//                    (b) =SAVINGS_M_CONTR(10000,C1,500,D2) <br>
-//                    (c) =SAVINGS_M_CONTR(C1,B1,A1,G10)
-//                """;
-//    }
-//
-//    @Override
-//    public String getName() {
-//        return "SAVINGS_M_CONTR";
-//    }
-//}
+package com.bervan.spreadsheet.functions;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.bervan.spreadsheet.functions.SavingsWithMonthlyContribution.FUNCTION_NAME;
+
+@Component(FUNCTION_NAME)
+public class SavingsWithMonthlyContribution implements SpreadsheetFunction {
+    public final static String FUNCTION_NAME = "SAVINGS_M_CONTR";
+
+    private double calculateFutureValueWithMonthlyContributions(double initialCapital, double monthlyContribution, double annualInterestRate, double years) {
+        double monthlyRate = annualInterestRate / 12;
+        double totalMonths = years * 12;
+        double futureValue = initialCapital * Math.pow(1 + monthlyRate, totalMonths);
+
+        for (int i = 1; i <= totalMonths; i++) {
+            futureValue += monthlyContribution * Math.pow(1 + monthlyRate, totalMonths - i);
+        }
+
+        return futureValue;
+    }
+
+    @Override
+    public FunctionArgument calculate(List<FunctionArgument> functionArguments) {
+        if (functionArguments.size() != 4) {
+            throw new RuntimeException("Incorrect Params for " + FUNCTION_NAME + " function");
+        }
+
+        return new ConstantArgument(
+                String.valueOf(calculateFutureValueWithMonthlyContributions(functionArguments.get(0).asDouble(),
+                        functionArguments.get(1).asDouble(),
+                        functionArguments.get(2).asDouble(),
+                        functionArguments.get(3).asDouble()))
+        );
+    }
+
+    @Override
+    public String getInfo() {
+        return """
+                Examples: <br>
+                Total savings after 10 years with initial capital 10000 and 5% interest rate and 500 monthly contribution <br>
+                    (a) =SAVINGS_M_CONTR(10000,0.05,500,10) <br>
+                    (b) =SAVINGS_M_CONTR(10000,C1,500,D2) <br>
+                    (c) =SAVINGS_M_CONTR(C1,B1,A1,G10)
+                """;
+    }
+
+    @Override
+    public String getName() {
+        return FUNCTION_NAME;
+    }
+}
