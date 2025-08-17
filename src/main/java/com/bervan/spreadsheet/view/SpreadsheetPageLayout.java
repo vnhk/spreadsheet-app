a955a8514fb7b6ca504dec3af01b53dc34b4a2c3
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:27:17 +0200

    New Navigation menu styling

--- src/main/java/com/bervan/spreadsheet/view/SpreadsheetPageLayout.java
+++ src/main/java/com/bervan/spreadsheet/view/SpreadsheetPageLayout.java
@@ -1,14 +1,15 @@
 package com.bervan.spreadsheet.view;
 
 import com.bervan.common.MenuNavigationComponent;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 public class SpreadsheetPageLayout extends MenuNavigationComponent {
     public SpreadsheetPageLayout(boolean isEdit, String spreadsheetName) {
         super(AbstractSpreadsheetsView.ROUTE_NAME);
 
-        addButtonIfVisible(menuButtonsRow, AbstractSpreadsheetsView.ROUTE_NAME, "List");
+        addButtonIfVisible(menuButtonsRow, AbstractSpreadsheetsView.ROUTE_NAME, "List", VaadinIcon.HOME.create());
         if (isEdit) {
-            addButtonIfVisible(menuButtonsRow, AbstractSpreadsheetView.ROUTE_NAME, spreadsheetName);
+            addButtonIfVisible(menuButtonsRow, AbstractSpreadsheetView.ROUTE_NAME, spreadsheetName, VaadinIcon.HOME.create());
         }
 
         add(menuButtonsRow);
